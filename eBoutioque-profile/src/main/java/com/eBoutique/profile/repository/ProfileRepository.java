c9ca6b034d2ae3546489154701ce25a7dee09ad8
mk <mk@Zensar-Macbook.local>
2019-08-07 14:03:43 -0400

    added a new method

--- eBoutioque-profile/src/main/java/com/eBoutique/profile/repository/ProfileRepository.java
+++ eBoutioque-profile/src/main/java/com/eBoutique/profile/repository/ProfileRepository.java
@@ -1,12 +1,20 @@
 package com.eBoutique.profile.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.eBoutique.profile.model.Profile;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long > {
+	
+	
+	@Query("SELECT u FROM Profile u WHERE u.login = ?1")
+
+	public Optional<Profile> findByEmail(String login);
 	
 }
 
