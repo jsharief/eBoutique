1f4ece8e6992de2c343c7e2a7c8f5bdc1815e252
mk <mk@Zensar-Macbook.local>
2019-08-07 14:13:16 -0400

    test

---
+++ eBoutioque-profile/src/main/java/com/eBoutique/profile/config/EBSecurityConfig.java
@@ -0,0 +1,27 @@
+///**
+// * 
+// */
+//package com.eBoutique.profile.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * @author eBoutique.in
+// *
+// */
+//
+////@EnableWebSecurity
+////@Configuration
+//public class EBSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//	
+//	@Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // For example: Use only Http Basic and not form login.
+//        
+//        http.authorizeRequests().anyRequest().permitAll();
+//    }
+//}
--- eBoutioque-profile/src/main/java/com/eBoutique/profile/services/EBProfileServiceImpl.java
+++ eBoutioque-profile/src/main/java/com/eBoutique/profile/services/EBProfileServiceImpl.java
@@ -1,12 +1,15 @@
 package com.eBoutique.profile.services;
 
 import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import com.eBoutique.profile.model.Profile;
+import com.eBoutique.profile.repository.ProfileRepository;
 
 /**
  * @author mk
@@ -29,6 +32,19 @@ public class EBProfileServiceImpl implements ProfileService {
 		pProfile.setRegistrationDate(new Date());
 		profileRepository.save(pProfile);
 		 
+	}
+	
+	/**
+	 * 
+	 * @param profile
+	 * @return
+	 */
+	public Optional<Profile> isProfileExist(Profile profile) {
+		 
+		Optional<Profile> optional = ((ProfileRepository)profileRepository).findByEmail(profile.getLogin());
+		 
+		return optional;
+		
 	}
 
 }
